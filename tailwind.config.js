cec34291626c1c24a427e297f7b50694e7dd06b3
cleverprogrammer <cleverprogrammer.com@gmail.com>
2022-10-27 05:34:39 +0100

    project 5 tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "green": "#1CE783",
+        "lightgreen": "#13854D",
+        "black11" : "#0B0C0F"
+      },
+      spacing: {
+        "yy": "53rem",
+        "more" : "7",
+        "h-98": "29rem"
+      }
+    
+    },
+  },
+  plugins: [],
+}
